5df3abc0b04fd6ac5d219fa1b37ccc8d31fac06c
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-05-30 18:48:59 +0200

    Update CalculoDni.java
    Added try-catch

--- ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici2/src/s104n1e2/CalculoDni.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici2/src/s104n1e2/CalculoDni.java
@@ -7,16 +7,19 @@ public class CalculoDni {
 
 	}
 	
-
 	public char calcularLletra(int dniNum) {
 		char lletraDni = '-';
 		int moduloDni = dniNum % 23;
 
-		if ((dniNum / 10000000 >= 1) && (dniNum / 100000000 < 1) && dniNum != 0) {
-			String charList = "TRWAGMYFPDXBNJZSQVHLCKE";
-			lletraDni = charList.charAt(moduloDni);
-		} else {
-			System.out.println("El número de DNI "+ dniNum+ " no és un valor correcte. ");
+		try {
+			if ((dniNum / 10000000 >= 1) && (dniNum / 100000000 < 1) && dniNum != 0) {
+				String charList = "TRWAGMYFPDXBNJZSQVHLCKE";
+				lletraDni = charList.charAt(moduloDni);
+			} else {
+				System.out.println("El número de DNI " + dniNum + " no és un valor correcte. ");
+			}
+		} catch (InputMismatchException e) {
+			System.out.println("Error de format. ");
 		}
 
 		return lletraDni;
