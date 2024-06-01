544b06b05d0d925e57230d40873382c68e209652
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-02 00:40:24 +0200

    S104N3E4
    first commit.
    
    Test working as expected.

---
+++ ItAcademyJava/Sprint1/Tasca04/Nivell3/Exercici4/src/s104n3e4/Exercici4Test.java
@@ -0,0 +1,27 @@
+package s104n3e4;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class Exercici4Test {
+
+	@Test
+	public void exercici4Test() {
+		ArrayList<Object> llista = new ArrayList<Object>();
+		String str1 = "String for testing";
+		String str2 = "Second string for testing";
+		int num1 = 5;
+		byte num2 = 12;
+		llista.add(str1);
+		llista.add(num1);
+		llista.add(num2);
+		assertThat(llista).contains(str1, num1, num2);
+		assertThat(llista).containsOnlyOnce(str1);
+		assertThat(llista).doesNotContain(str2);
+
+}
+	
+}
