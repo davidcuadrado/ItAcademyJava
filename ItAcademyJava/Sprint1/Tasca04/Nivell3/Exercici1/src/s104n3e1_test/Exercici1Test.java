ad37256753a317bec14677c2b9a4c45bc1ac711d
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-04 11:57:54 +0200

    Update Exercici1Test.java
    Method names modification.

--- ItAcademyJava/Sprint1/Tasca04/Nivell3/Exercici1/src/s104n3e1_test/Exercici1Test.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell3/Exercici1/src/s104n3e1_test/Exercici1Test.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class Exercici1Test {
 
 	@Test
-	public void exercici1EqualTest() {
+	public void equalTest() {
 		int num1 = (int) 8;
 		int num2 = (int) 8;
 		assertThat(num1).isEqualTo(num2);
@@ -16,7 +16,7 @@ class Exercici1Test {
 	}
 	
 	@Test
-	public void exercici1NotEqualTest() {
+	public void notEqualTest() {
 		int num1 = (int) 5;
 		int num2 = (int) 8;
 	//	assertThat(num1).isEqualTo(num2);
