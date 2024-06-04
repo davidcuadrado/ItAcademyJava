7c53924beeebd203f9942d11bae56ba246b3e441
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-04 23:46:31 +0200

    S106N2E2 first commit
    First commit

---
+++ ItAcademyJava/Sprint1/Tasca06/Nivell2/Exercici2/src/s106n2e2/UndefinedTypeParameterization.java
@@ -0,0 +1,14 @@
+package s106n2e2;
+
+
+public class UndefinedTypeParameterization {
+	
+	public <T> void genericMethodUndefinedParameters(Object parameter1, Object parameter2, Object parameter3) {
+
+		System.out.println(parameter1);
+		System.out.println(parameter2);
+		System.out.println(parameter3 + "\n");
+
+	}
+
+}
