54743cdecd698e08baff612490a1e54fa115e796
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-05 00:05:30 +0200

    Updated NoGenericMethods
    Re-did whole exercise.

--- ItAcademyJava/Sprint1/Tasca06/Nivell1/Exercici1/src/s106n1e1/NoGenericMethods.java
+++ ItAcademyJava/Sprint1/Tasca06/Nivell1/Exercici1/src/s106n1e1/NoGenericMethods.java
@@ -2,45 +2,42 @@ package s106n1e1;
 
 public class NoGenericMethods {
 	
-	private Object object1;
-	private Object object2;
-	private Object object3;
+	private String subjecte;
+	private String verb;
+	private String predicat;
 	
 	
-	public NoGenericMethods(Object object1, Object object2, Object object3) {
-		this.object1 = object1;
-		this.object2 = object2;
-		this.object3 = object3;
+	public NoGenericMethods(String subjecte,String verb, String predicat) {
+		super();
+		this.subjecte = subjecte;
+		this.verb = verb;
+		this.predicat = predicat;
 	}
-
-
-	public Object getObject1() {
-		return object1;
+	
+	
+	public Object getSubjecte() {
+		return subjecte;
 	}
-
-
-	public void setObject1(Object object1) {
-		this.object1 = object1;
+	public void setSubjecte(String subjecte) {
+		this.subjecte = subjecte;
 	}
-
-
-	public Object getObject2() {
-		return object2;
+	public Object getVerb() {
+		return verb;
 	}
-
-
-	public void setObject2(Object object2) {
-		this.object2 = object2;
+	public void setVerb(String verb) {
+		this.verb = verb;
 	}
-
-
-	public Object getObject3() {
-		return object3;
+	public Object getPredicat() {
+		return predicat;
 	}
-
-
-	public void setObject3(Object object3) {
-		this.object3 = object3;
+	public void setPredicat(String predicat) {
+		this.predicat = predicat;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "NoGenericMethods Prints: " + subjecte + ", " + verb + ", " + predicat + ". ";
 	}
 	
 	
