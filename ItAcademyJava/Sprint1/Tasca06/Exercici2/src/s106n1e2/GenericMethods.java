635969814182ebeaa6156a8a33b5479109af4f7c
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-04 13:46:02 +0200

    S106N1E2
    First commit
    
    Pending peer review

---
+++ ItAcademyJava/Sprint1/Tasca06/Exercici2/src/s106n1e2/GenericMethods.java
@@ -0,0 +1,16 @@
+package s106n1e2;
+
+public  class GenericMethods {
+	
+	
+	
+	public <T> void genericMethod(T parameter1, T parameter2, T parameter3){
+		
+		System.out.println(parameter1);
+		System.out.println(parameter2);
+		System.out.println(parameter3+"\n");
+		
+		
+	}
+
+}
