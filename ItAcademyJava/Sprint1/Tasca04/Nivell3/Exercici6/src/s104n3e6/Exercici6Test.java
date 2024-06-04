a52bc7d242468ac3aaf47b3e1eb7e49c6aa0245b
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-04 12:00:42 +0200

    Update Exercici6Test.java
    Method name modification.

--- ItAcademyJava/Sprint1/Tasca04/Nivell3/Exercici6/src/s104n3e6/Exercici6Test.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell3/Exercici6/src/s104n3e6/Exercici6Test.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 class Exercici6Test {
 
 	@Test
-	void outOfBoundTest() throws ArrayIndexOutOfBoundsException{
+	void outOfBoundsTest() throws ArrayIndexOutOfBoundsException{
 		Lanzador lanzador1 = new Lanzador();
 		
 		assertThatThrownBy(lanzador1::lanzadorIndexOutOfBounds).isInstanceOf(ArrayIndexOutOfBoundsException.class);
