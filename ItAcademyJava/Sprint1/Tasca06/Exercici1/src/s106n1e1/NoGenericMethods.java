6bf1b0fc517695a530d52ac2e291f349c076d7d7
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-04 13:14:01 +0200

    S106 First commit
    First commit on S106
    First commit on S106N1E1

---
+++ ItAcademyJava/Sprint1/Tasca06/Exercici1/src/s106n1e1/NoGenericMethods.java
@@ -0,0 +1,50 @@
+package s106n1e1;
+
+public class NoGenericMethods {
+	
+	private Object object1;
+	private Object object2;
+	private Object object3;
+	
+	
+	public NoGenericMethods(Object object1, Object object2, Object object3) {
+		this.object1 = object1;
+		this.object2 = object2;
+		this.object3 = object3;
+	}
+
+
+	public Object getObject1() {
+		return object1;
+	}
+
+
+	public void setObject1(Object object1) {
+		this.object1 = object1;
+	}
+
+
+	public Object getObject2() {
+		return object2;
+	}
+
+
+	public void setObject2(Object object2) {
+		this.object2 = object2;
+	}
+
+
+	public Object getObject3() {
+		return object3;
+	}
+
+
+	public void setObject3(Object object3) {
+		this.object3 = object3;
+	}
+	
+	
+	
+	
+
+}
