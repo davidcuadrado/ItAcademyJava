62460ebbafa4e8ffc3562d7eef58b20cf838330a
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-05 10:41:21 +0200

    Update Gestion.java
    Corregido post-peer review

--- ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici1/src/s104n1e1/Gestion.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici1/src/s104n1e1/Gestion.java
@@ -1,39 +1,29 @@
 package s104n1e1;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Gestion {
+	
 
-	public ArrayList<Month> monthListGen() {
-		ArrayList<Month> monthList = new ArrayList<Month>();
+	public List<String> monthListGen() {
+		List<String> monthList = new ArrayList<>();
 
-		Month gener = new Month("gener");
-		Month febrer = new Month("febrer");
-		Month març = new Month("març");
-		Month abril = new Month("abril");
-		Month maig = new Month("maig");
-		Month juny = new Month("juny");
-		Month juliol = new Month("juliol");
-		Month agost = new Month("agost");
-		Month setembre = new Month("setembre");
-		Month octubre = new Month("octubre");
-		Month novembre = new Month("novembre");
-		Month desembre = new Month("desembre");
-
-		monthList.add(gener);
-		monthList.add(febrer);
-		monthList.add(març);
-		monthList.add(abril);
-		monthList.add(maig);
-		monthList.add(juny);
-		monthList.add(juliol);
-		monthList.add(agost);
-		monthList.add(setembre);
-		monthList.add(octubre);
-		monthList.add(novembre);
-		monthList.add(desembre);
+		monthList.add("gener");
+		monthList.add("febrer");
+		monthList.add("març");
+		monthList.add("abril");
+		monthList.add("maig");
+		monthList.add("juny");
+		monthList.add("juliol");
+		monthList.add("agost");
+		monthList.add("setembre");
+		monthList.add("octubre");
+		monthList.add("novembre");
+		monthList.add("desembre");
 		
-	return monthList;
+		return monthList;
+
 	}
 	
 }
