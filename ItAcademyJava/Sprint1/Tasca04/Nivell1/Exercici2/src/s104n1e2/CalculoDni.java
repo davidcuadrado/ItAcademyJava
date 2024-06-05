5397cfc855161da11b8c6c587a7ad1a525265924
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-05 12:27:26 +0200

    Update CalculoDni.java
    Updated after peer review.

--- ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici2/src/s104n1e2/CalculoDni.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici2/src/s104n1e2/CalculoDni.java
@@ -1,25 +1,20 @@
 package s104n1e2;
 
+import java.lang.reflect.Array;
 
 public class CalculoDni {
 
-	public CalculoDni() {
-
-	}
-	
 	public char calcularLletra(int dniNum) {
 		char lletraDni = '-';
 		int moduloDni = dniNum % 23;
+		// final String charList = "TRWAGMYFPDXBNJZSQVHLCKE";
+		final char[] letras = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B',
+	        	'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
 
-		try {
-			if ((dniNum / 10000000 >= 1) && (dniNum / 100000000 < 1) && dniNum != 0) {
-				String charList = "TRWAGMYFPDXBNJZSQVHLCKE";
-				lletraDni = charList.charAt(moduloDni);
-			} else {
-				System.out.println("El número de DNI " + dniNum + " no és un valor correcte. ");
-			}
-		} catch (InputMismatchException e) {
-			System.out.println("Error de format. ");
+		if ((dniNum / 10000000 >= 1) && (dniNum / 100000000 < 1) && dniNum != 0) {
+			lletraDni = (char) Array.getChar(letras, moduloDni);
+		} else {
+			System.out.println("El número de DNI "+ dniNum+ " no és un valor correcte. ");
 		}
 
 		return lletraDni;
