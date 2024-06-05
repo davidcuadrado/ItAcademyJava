8a7c083dfa8da8b842893282ff0ffb5952a3b739
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-05 10:42:36 +0200

    Update MonthArrayTest.java
    Updated after peer review changes

--- ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici1/src/test/MonthArrayTest.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici1/src/test/MonthArrayTest.java
@@ -1,7 +1,7 @@
 package test;
 
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import s104n1e1.*;
@@ -13,29 +13,33 @@ class MonthArrayTest {
 	@BeforeEach
 	public void setUp() {
 	gestion1 = new Gestion();
+	gestion1.monthListGen();
 	}
 	
 	
 	@Test
 	public void testPositionsMax() {
-		ArrayList<Month> monthList = gestion1.monthListGen();
+		gestion1 = new Gestion();
+		List<String> monthList = gestion1.monthListGen();
 		System.out.println("La llista té 12 posicions: ");
 		Assertions.assertTrue(monthList.size() == 12);
 	}
 	
 	@Test
 	public void testNull() {
-		ArrayList<Month> monthList = gestion1.monthListGen();
+		gestion1 = new Gestion();
+		List<String> monthList = gestion1.monthListGen();
 		System.out.println("La llista amb els mesos no és nul·la: ");
 		Assertions.assertNotNull(monthList);
 	}
 	
 	@Test
 	public void testAgost8() {
-		ArrayList<Month> monthList = gestion1.monthListGen();
+		gestion1 = new Gestion();
+		List<String> monthList = gestion1.monthListGen();
 		System.out.println("En la seva posició 8 conté \"agost\": ");
 		//Nos lanza un failure ya que la posición del mes "agost" es 7
-		Assertions.assertEquals(monthList.get(8).getMonthName(), "agost");
+		Assertions.assertEquals(monthList.get(8), "agost");
 	}
 	
 
