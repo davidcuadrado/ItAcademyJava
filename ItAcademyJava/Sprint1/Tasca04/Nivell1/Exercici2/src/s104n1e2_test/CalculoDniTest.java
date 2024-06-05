790493dfa4db4235a2350d2b03b7f2de6b580ff3
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-05 12:25:58 +0200

    Update CalculoDniTest.java
    Feat: updated after peer review

--- ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici2/src/s104n1e2_test/CalculoDniTest.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici2/src/s104n1e2_test/CalculoDniTest.java
@@ -2,48 +2,48 @@ package s104n1e2_test;
 
 import s104n1e2.*;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CalculoDniTest {
 
 	CalculoDni calculoDni1 = new CalculoDni();
-	ArrayList<Integer> dniList = new ArrayList<Integer>();
-	ArrayList<String> dniStringList = new ArrayList<String>();
+
+	private static Stream<Arguments> dniPredefinits() {
+		return Stream.of(Arguments.of(345864985, 'P'), Arguments.of(45926775, 'J'), Arguments.of(34865971, 'L'),
+				Arguments.of(61248537, 'R'), Arguments.of(35694528, 'T'), Arguments.of(84532699, 'N'),
+				Arguments.of(75426485, 'K'), Arguments.of(326458521, 'V'), Arguments.of(32654851, 'S'),
+				Arguments.of(75426589, 'G'));
+	}
 
 	@BeforeEach
 	public void setUp() {
-		dniList = null;
 		calculoDni1 = new CalculoDni();
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = { 0000, 0000000000, 100000000, 9999, 99999999, 999999999, 65849235, 94586254, 25684975,
-			246589512 })
-	public void typeTest(int dniNum) {
-		char dniChar = calculoDni1.calcularLletra(dniNum);
+	@MethodSource("dniPredefinits")
+	public void typeTest(int dniNum, char dniChar) {
+		char lletraEsperada = calculoDni1.calcularLletra(dniNum);
 		Assertions.assertTrue(Character.class.isInstance(dniChar));
+		Assertions.assertTrue(Character.class.isInstance(lletraEsperada));
 		Assertions.assertTrue(Integer.class.isInstance(dniNum));
 
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = { 0000, 0000000000, 100000000, 9999, 99999999, 999999999, 65849235, 9458625, 25684975,
-			246589512 })
-	public void lengthTest(int dniNum) {
-		char dniChar = calculoDni1.calcularLletra(dniNum);
-		String dni = (String.valueOf(dniNum) + dniChar);
+	@MethodSource("dniPredefinits")
+	public void lengthTest(int dniNum, char dniChar) {
+		char lletraEsperada = calculoDni1.calcularLletra(dniNum);
+		String dni = (String.valueOf(dniNum) + lletraEsperada);
 		// Provoca failure en los valores dniNum != 8
 		if ((dniNum / 10000000 >= 1) && (dniNum / 100000000 < 1) && dniNum != 0) {
-		Assertions.assertEquals(dni.length(), 9);
+			Assertions.assertEquals(dni.length(), 9);
 		} else {
 			Assertions.assertNotEquals(dni.length(), 9);
 		}
@@ -51,39 +51,19 @@ class CalculoDniTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = { 0000, 0000000000, 100000000, 9999, 99999999, 999999999, 65849235, 94586254, 25684975,
-			246589512 })
-	public void dniCorrectTest(int dniNum) {
-		char dniChar = calculoDni1.calcularLletra(dniNum);
-		String dni = (String.valueOf(dniNum) + dniChar);
+	@MethodSource("dniPredefinits")
+	public void dniToStringTest(int dniNum, char dniChar) {
+		char lletraEsperada = calculoDni1.calcularLletra(dniNum);
+		String dni = (String.valueOf(dniNum) + lletraEsperada);
 		Assertions.assertTrue(String.class.isInstance(dni));
 
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = { 0000, 0000000000, 100000000, 9999, 99999999, 999999999, 65849235, 94586254, 25684975,
-			246589512 })
-	public void charTest(int dniNum) {
-		char dniChar = calculoDni1.calcularLletra(dniNum);
-
-		char lletraDni = '-';
-		int moduloDni = dniNum % 23;
-		String line = null;
-
-		if ((dniNum / 10000000 >= 1) && (dniNum / 100000000 < 1)) {
-			try {
-				File conversor = new File("src\\s104n1e2_conversor\\conversor.txt");
-				BufferedReader reader = new BufferedReader(new FileReader(conversor));
-				for (int i = 0; i <= moduloDni; i++) {
-					line = reader.readLine();
-				}
-				reader.close();
-			} catch (IOException e) {
-				System.out.println("Error en la lectura del document. ");
-			}
-			lletraDni = line.charAt(0);
-		}
-		Assertions.assertEquals(dniChar, lletraDni);
+	@MethodSource("dniPredefinits")
+	public void charTest(int dniNum, char dniChar) {
+		char lletraEsperada = calculoDni1.calcularLletra(dniNum);
+		Assertions.assertEquals(dniChar, lletraEsperada);
 	}
 
 }
