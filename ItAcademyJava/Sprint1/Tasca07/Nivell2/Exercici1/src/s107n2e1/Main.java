7d7b0b687795b971f53dfcbf95b9c54de882a757
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-09 19:28:26 +0200

    S107N2E1
    First commit.
    
    Pending: solve text output issues to JSON
    Pending: meta-annotations

---
+++ ItAcademyJava/Sprint1/Tasca07/Nivell2/Exercici1/src/s107n2e1/Main.java
@@ -0,0 +1,11 @@
+package s107n2e1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Serializador.serializarPersona();
+
+	}
+
+}
---
+++ ItAcademyJava/Sprint1/Tasca07/Nivell2/Exercici1/src/s107n2e1/Serializador.java
@@ -0,0 +1,48 @@
+package s107n2e1;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Serializador {
+
+	public static void serializarPersona() {
+		Persona personaAdd = personaDatos();
+
+		if (personaAdd.getClass().isAnnotationPresent(Serializar.class)) {
+			Serializar persona = personaAdd.getClass().getAnnotation(Serializar.class);
+			String rutaArchivo = persona.rutaArchivo();
+			File personaSerializada = new File(rutaArchivo, personaAdd.getClass()+ ".txt");
+
+			try (FileOutputStream serializandoPersona = new FileOutputStream(personaSerializada, true);
+					ObjectOutputStream finSerializacion = new ObjectOutputStream(serializandoPersona)){
+				finSerializacion.writeObject(personaAdd);
+			} catch (IOException e) {
+				System.out.println("Error en la escritura del archivo. ");
+			}
+		}
+	}
+	
+	public static Persona personaDatos() {
+		Persona personaAdd = null;
+		
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.print("Introdueix el nom: ");
+				String nom = sc.nextLine();
+				System.out.print("Introdueix el cognom: ");
+				String cognom = sc.nextLine();
+				System.out.print("Introdueix l'edat: ");
+				int edat = sc.nextInt();
+				
+				personaAdd = new Persona(nom, cognom, edat);
+					
+				} catch (InputMismatchException e) {
+					System.out.println("Error en la introducción de datos. ");
+				}
+		return personaAdd;
+	}
+}
+
