88c9f5e93a352fb77b795fcc9283bb92a86bd3ab
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-10 11:23:42 +0200

    S108N1E3 first commit

---
+++ ItAcademyJava/Sprint1/Tasca08/Nivell1/Exercici3/src/s108n1e3/Month.java
@@ -0,0 +1,22 @@
+package s108n1e3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Month {
+
+	public static void main(String[] args) {
+
+		List<String> monthList = Arrays.asList("gener", "febrer", "març", "abril", "maig", "juny", "juliol", "agost",
+				"setembre", "octubre", "novembre", "desembre");
+		
+		printMonth(monthList);
+		
+	}
+	
+	public static void printMonth(List<String> monthList) {
+		
+		monthList.forEach((s) -> System.out.println(s));
+	}
+
+}
