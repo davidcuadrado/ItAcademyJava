a05db4f39c1c556a62713ca8fa498591de9336a0
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-10 19:10:05 +0200

    Update Gestion.java
    Updated List to add months in the same line.

--- ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici1/src/s104n1e1/Gestion.java
+++ ItAcademyJava/Sprint1/Tasca04/Nivell1/Exercici1/src/s104n1e1/Gestion.java
@@ -1,27 +1,14 @@
 package s104n1e1;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Gestion {
 	
 
 	public List<String> monthListGen() {
-		List<String> monthList = new ArrayList<>();
+		List<String> monthList = Arrays.asList("gener", "febrer", "març", "abril", "maig", "juny", "juliol", "agost", "setembre", "octubre", "novembre", "desembre");
 
-		monthList.add("gener");
-		monthList.add("febrer");
-		monthList.add("març");
-		monthList.add("abril");
-		monthList.add("maig");
-		monthList.add("juny");
-		monthList.add("juliol");
-		monthList.add("agost");
-		monthList.add("setembre");
-		monthList.add("octubre");
-		monthList.add("novembre");
-		monthList.add("desembre");
-		
 		return monthList;
 
 	}
