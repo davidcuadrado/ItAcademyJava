ad7dc7155dc0eb179c32a3cb351485272c0e6085
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-10 11:00:31 +0200

    Update ListaO.java
    Format

--- ItAcademyJava/Sprint1/Tasca08/Nivell1/Exercici1/src/s108n1e1/ListaO.java
+++ ItAcademyJava/Sprint1/Tasca08/Nivell1/Exercici1/src/s108n1e1/ListaO.java
@@ -7,22 +7,20 @@ import java.util.stream.Collectors;
 public class ListaO {
 
 	public static void main(String[] args) {
-		
+
 		List<String> wordList = Arrays.asList("oso", "cabra", "elefante", "lobo", "pelicano", "lobo");
 		List<String> wordListO = screenO(wordList);
-		
+
 		for (String wordO : wordListO) {
 			System.out.println(wordO);
 		}
 
 	}
-	
-	public static List<String> screenO(List<String> wordList){
-		
-		List<String> wordListO = wordList.stream()
-						.filter( word -> word.contains("o"))
-						.collect(Collectors.toList());
-						
+
+	public static List<String> screenO(List<String> wordList) {
+
+		List<String> wordListO = wordList.stream().filter(word -> word.contains("o")).collect(Collectors.toList());
+
 		return wordListO;
 	}
 
