e3218c5411451061bde146b762feb4d2b119a192
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-10 14:33:43 +0200

    Actualizar Program.java
    Test pending. Lines added without test.

--- ItAcademyJava/Sprint1/Tasca08/Nivell1/Exercici8/src/s108n1e8/Program.java
+++ ItAcademyJava/Sprint1/Tasca08/Nivell1/Exercici8/src/s108n1e8/Program.java
@@ -3,8 +3,14 @@ package s108n1e8;
 public class Program {
 
 	public static void main(String[] args) {
+
+   String stringToReverse = "A new string ready to be reversed."
 		
-		Reverser reverser = ( (String) -> { String.toCharArray();
+		Reverser reverser = () -> new StringBuilder(new String(stringToReverse)).reverse().toString();
+
+String stringReversed = reverser.reverse(stringToReverse);
+
+System.out.println(stringReversed);
 			
 			
 		} );
