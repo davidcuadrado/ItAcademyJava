b02fe3eb0cdbef0decb68d26b5357851db5a6cb8
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-10 14:00:42 +0200

    S108N1E8 first commit
    Pending

---
+++ ItAcademyJava/Sprint1/Tasca08/Nivell1/Exercici8/src/s108n1e8/Program.java
@@ -0,0 +1,14 @@
+package s108n1e8;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Reverser reverser = ( (String) -> { String.toCharArray();
+			
+			
+		} );
+
+	}
+
+}
---
+++ ItAcademyJava/Sprint1/Tasca08/Nivell1/Exercici8/src/s108n1e8/Reverser.java
@@ -0,0 +1,8 @@
+package s108n1e8;
+
+@FunctionalInterface
+public interface Reverser {
+	
+	public String reverse(String word);
+
+}
