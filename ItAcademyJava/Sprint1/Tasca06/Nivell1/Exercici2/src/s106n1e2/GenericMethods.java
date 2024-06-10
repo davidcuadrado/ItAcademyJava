a664dc22f24e59626e8d3ab09c1117db2b6f1c2e
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-10 18:25:04 +0200

    Update GenericMethods.java
    Updated generic types.

--- ItAcademyJava/Sprint1/Tasca06/Nivell1/Exercici2/src/s106n1e2/GenericMethods.java
+++ ItAcademyJava/Sprint1/Tasca06/Nivell1/Exercici2/src/s106n1e2/GenericMethods.java
@@ -2,15 +2,10 @@ package s106n1e2;
 
 public  class GenericMethods {
 	
-	
-	
-	public <T> void genericMethod(T parameter1, T parameter2, T parameter3){
-		
+	public <T, U, V> void genericMethod(T parameter1, U parameter2, V parameter3){
 		System.out.println(parameter1);
 		System.out.println(parameter2);
 		System.out.println(parameter3+"\n");
-		
-		
 	}
 
 }
