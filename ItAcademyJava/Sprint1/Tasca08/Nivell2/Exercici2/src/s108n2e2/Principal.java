687d30fca016012a0b185c92ac6be750d129b5be
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-11 02:01:24 +0200

    S108N2E2 first commit

---
+++ ItAcademyJava/Sprint1/Tasca08/Nivell2/Exercici2/src/s108n2e2/Principal.java
@@ -0,0 +1,29 @@
+package s108n2e2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		List<Integer> numList = Arrays.asList(3, 55, 44, 95, 48, 22);
+		
+		String formatedString = numSeparator(numList);
+		
+		System.out.println(formatedString);
+
+	}
+	
+	public static String numSeparator(List<Integer>numList) {
+		
+		List<String> formatingString = numList.stream().map(num -> (num % 2 == 0 ? "o"+num : "e" +num)).collect(Collectors.toList());
+		String formatedString = String.join(", ", formatingString);
+		
+		return formatedString;
+		
+	}
+
+}
