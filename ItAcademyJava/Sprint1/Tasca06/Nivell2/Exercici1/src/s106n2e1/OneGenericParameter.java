45490e988630450e97edf3cfadc28e368b4eab2e
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-11 11:37:03 +0200

    Update OneGenericParameter.java
    Peer reviewed
    
    Updated with correction on parameter type.

--- ItAcademyJava/Sprint1/Tasca06/Nivell2/Exercici1/src/s106n2e1/OneGenericParameter.java
+++ ItAcademyJava/Sprint1/Tasca06/Nivell2/Exercici1/src/s106n2e1/OneGenericParameter.java
@@ -3,7 +3,7 @@ package s106n2e1;
 public class OneGenericParameter {
 
 	
-	public <T> void genericMethodOneString(String parameter1, T parameter2, T parameter3) {
+	public <T, U> void genericMethodOneString(String parameter1, T parameter2, U parameter3) {
 
 		System.out.println(parameter1);
 		System.out.println(parameter2);
