ca44da5ad8842736c34317a05b5e2029f1b05a1d
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-12 05:58:38 +0200

    Update S108N2E4 Principal.java
    Created printApartado methods for releasing charge on main.

--- ItAcademyJava/Sprint1/Tasca08/Nivell2/Exercici4/src/s108n2e4/Principal.java
+++ ItAcademyJava/Sprint1/Tasca08/Nivell2/Exercici4/src/s108n2e4/Principal.java
@@ -9,23 +9,36 @@ public class Principal {
 	public static void main(String[] args) {
 		List<Object> mixedList = List.of("fire", "earth", 5, 18, "day", 9, "water", 7, "night", "wind");
 
+		printApartado1(mixedList);
+		printApartado2(mixedList);
+		printApartado3(mixedList);
+		printApartado4(mixedList);
+
+	}
+
+	public static void printApartado1(List<Object> mixedList) {
 		System.out.println("Apartado 1: \n");
 		mixedList = orderListAlphabetically(mixedList);
 		mixedList.forEach(System.out::println);
 		// se podría filtrar para que 18 no aparezca el primero?
+	}
 
+	public static void printApartado2(List<Object> mixedList) {
 		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _\nApartado 2: \n");
-		List<Object> mixedListAlphabeticallyAndE = orderListAlphabeticallyAndE(mixedList);
+		List<Object> mixedListAlphabeticallyAndE = orderListEFirst(mixedList);
 		mixedListAlphabeticallyAndE.forEach(System.out::println);
+	}
 
+	public static void printApartado3(List<Object> mixedList) {
 		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _\nApartado 3: \n");
-		List<Object> mixedListATo4 = mixedListReplaceATo4(mixedList);
+		List<Object> mixedListATo4 = replaceATo4(mixedList);
 		mixedListATo4.forEach(System.out::println);
+	}
 
+	public static void printApartado4(List<Object> mixedList) {
 		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _\nApartado 4: \n");
 		List<Object> mixedListNum = mixedListToNum(mixedList);
 		mixedListNum.forEach(System.out::println);
-
 	}
 
 	public static List<Object> orderListAlphabetically(List<Object> mixedList) {
@@ -33,9 +46,9 @@ public class Principal {
 		return mixedList.stream().sorted(Comparator.comparing(Object::toString)).toList();
 	}
 
-	public static List<Object> orderListAlphabeticallyAndE(List<Object> mixedList) {
+	public static List<Object> orderListEFirst(List<Object> mixedList) {
 
-		List<String> mixedListToString = convertListToString(mixedList);
+		List<String> mixedListToString = convertListObjToString(mixedList);
 
 		List<String> mixedListStringE = mixedListToString.stream().filter(word -> word.contains("e")).toList();
 		List<String> mixedListStringNotE = mixedListToString.stream().filter(word -> !(word.contains("e"))).toList();
@@ -47,9 +60,9 @@ public class Principal {
 		return mixedListAlphabeticallyAndE;
 	}
 
-	public static List<Object> mixedListReplaceATo4(List<Object> mixedList) {
+	public static List<Object> replaceATo4(List<Object> mixedList) {
 
-		List<String> mixedListToString = convertListToString(mixedList);
+		List<String> mixedListToString = convertListObjToString(mixedList);
 		mixedListToString = mixedListToString.stream().map(n -> n.replace('a', '4')).toList();
 		List<Object> mixedListATo4 = new ArrayList<Object>();
 		mixedListToString.forEach(n -> mixedListATo4.add(n));
@@ -59,7 +72,7 @@ public class Principal {
 
 	public static List<Object> mixedListToNum(List<Object> mixedList) {
 
-		List<String> mixedListToString = convertListToString(mixedList);
+		List<String> mixedListToString = convertListObjToString(mixedList);
 		List<Object> mixedListNum = new ArrayList<Object>();
 		mixedListToString = mixedListToString.stream().filter(n -> n.matches("\\d+")).toList();
 		mixedListToString.forEach(n -> mixedListNum.add(n));
@@ -67,7 +80,7 @@ public class Principal {
 		return mixedListNum;
 	}
 
-	public static List<String> convertListToString(List<Object> mixedList) {
+	public static List<String> convertListObjToString(List<Object> mixedList) {
 
 		List<String> mixedListString = new ArrayList<String>();
 
