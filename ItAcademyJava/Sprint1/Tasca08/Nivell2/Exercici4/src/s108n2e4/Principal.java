69f0e0514af9f355b135f0cd3c17de37e3982fd5
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-12 05:18:40 +0200

    Update Principal.java
    Format

--- ItAcademyJava/Sprint1/Tasca08/Nivell2/Exercici4/src/s108n2e4/Principal.java
+++ ItAcademyJava/Sprint1/Tasca08/Nivell2/Exercici4/src/s108n2e4/Principal.java
@@ -18,25 +18,21 @@ public class Principal {
 		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _\nApartado 2: \n");
 		List<Object> mixedListAlphabeticallyAndE = orderListAlphabeticallyAndE(mixedList);
 		mixedListAlphabeticallyAndE.forEach(System.out::println);
-		
+
 		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _\nApartado 3: \n");
 		List<Object> mixedListATo4 = mixedListReplaceATo4(mixedList);
 		mixedListATo4.forEach(System.out::println);
-		
+
 		System.out.println("_ _ _ _ _ _ _ _ _ _ _ _ _\nApartado 4: \n");
 		List<Object> mixedListNum = mixedListToNum(mixedList);
 		mixedListNum.forEach(System.out::println);
 
 	}
-	
-	
 
 	public static List<Object> orderListAlphabetically(List<Object> mixedList) {
 
 		return mixedList.stream().sorted(Comparator.comparing(Object::toString)).toList();
 	}
-	
-	
 
 	public static List<Object> orderListAlphabeticallyAndE(List<Object> mixedList) {
 
@@ -51,36 +47,33 @@ public class Principal {
 
 		return mixedListAlphabeticallyAndE;
 	}
-	
-	public static List<Object> mixedListReplaceATo4(List<Object> mixedList){
-		
+
+	public static List<Object> mixedListReplaceATo4(List<Object> mixedList) {
+
 		List<String> mixedListToString = convertListToString(mixedList);
 		mixedListToString = mixedListToString.stream().map(n -> n.replace('a', '4')).toList();
 		List<Object> mixedListATo4 = new ArrayList<Object>();
 		mixedListToString.forEach(n -> mixedListATo4.add(n));
-		
+
 		return mixedListATo4;
 	}
-	
-	public static List<Object> mixedListToNum(List<Object> mixedList){
-		
+
+	public static List<Object> mixedListToNum(List<Object> mixedList) {
+
 		List<String> mixedListToString = convertListToString(mixedList);
 		List<Object> mixedListNum = new ArrayList<Object>();
 		mixedListToString = mixedListToString.stream().filter(n -> n.matches("\\d+")).toList();
 		mixedListToString.forEach(n -> mixedListNum.add(n));
-		
+
 		return mixedListNum;
 	}
-	
-	
 
 	public static List<String> convertListToString(List<Object> mixedList) {
 
 		List<String> mixedListString = new ArrayList<String>();
 
 		mixedList.forEach(s -> mixedListString.add(s.toString()));
-		
+
 		return mixedListString;
 	}
-
 }
