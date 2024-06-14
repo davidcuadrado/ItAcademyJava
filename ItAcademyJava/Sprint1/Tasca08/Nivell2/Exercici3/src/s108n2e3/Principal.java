06879b9a9d5afee1b8e03352e7cabceed5c23054
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-14 03:58:52 +0200

    Update Principal.java
    Deleted interface method parameters

--- ItAcademyJava/Sprint1/Tasca08/Nivell2/Exercici3/src/s108n2e3/Principal.java
+++ ItAcademyJava/Sprint1/Tasca08/Nivell2/Exercici3/src/s108n2e3/Principal.java
@@ -6,17 +6,16 @@ public class Principal {
 
 		float num1 = 5f;
 		float num2 = 10f;
-		char tipoOper = '+';
+		char tipoOper = '*';
 
-		Operar operar = (float numA, float numB, char operador) -> switch (tipoOper) {
+		Operar operar = () -> switch (tipoOper) {
 		case '+' -> num1 + num2;
 		case '-' -> num1 - num2;
 		case '*' -> num1 * num2;
 		case '/' -> num1 / num2;
 		default -> throw new IllegalArgumentException("Unexpected value: " + tipoOper);
-
 		};
 
-		System.out.println("El teu resultat: " + num1 + tipoOper + num2 + " = " + operar.operacio(num1, num2, tipoOper));
+		System.out.println("El teu resultat: " + num1 + tipoOper + num2 + " = " + operar.operacio());
 	}
 }
