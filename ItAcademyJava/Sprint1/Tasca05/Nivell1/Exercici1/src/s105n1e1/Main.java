1ee7a6bff21a3b55bb7ce0723194e845e7f0ed4c
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-14 16:13:32 +0200

    Update Main.java
    Updated with Alfabetico.directory static reference.

--- ItAcademyJava/Sprint1/Tasca05/Nivell1/Exercici1/src/s105n1e1/Main.java
+++ ItAcademyJava/Sprint1/Tasca05/Nivell1/Exercici1/src/s105n1e1/Main.java
@@ -1,12 +1,8 @@
 package s105n1e1;
 
-
 public class Main {
 
 	public static void main(String[] args) {
-
-		Alfabetico.leerDirectorio();
-
-		
+		Alfabetico.ordenarDirectorio(Alfabetico.directory);
 	}
 }
