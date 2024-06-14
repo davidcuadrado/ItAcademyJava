2c2c7fce4b7e2bd356bdb2be49cfd08ee9987fa8
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-14 18:22:03 +0200

    S108N1E2 first commit
    Pending: readme.txt with commands indications.

---
+++ ItAcademyJava/Sprint1/Tasca05/Nivell1/src/s105n1e2/Principal.java
@@ -0,0 +1,10 @@
+package s105n1e2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Alfabetico.ordenarDirectorio(Alfabetico.directory);
+		Alfabetico.listarArbolDirectorio(Alfabetico.directoryFile);
+	}
+}
