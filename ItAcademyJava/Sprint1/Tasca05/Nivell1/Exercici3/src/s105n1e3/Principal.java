790da335451f61f7bd8ea4611bee1fcb2448b299
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-15 02:57:55 +0200

    S105N1E3 first commit

---
+++ ItAcademyJava/Sprint1/Tasca05/Nivell1/Exercici3/src/s105n1e3/Principal.java
@@ -0,0 +1,14 @@
+package s105n1e3;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Alfabetico.iniciar();
+
+		// Exercici1: Alfabetico.ordenarDirectorio(Alfabetico.directory);
+		// Exercici2 Alfabetico.listarArbolDirectorio(Alfabetico.directoryFile);
+		Alfabetico.txtArbolDirectorio(Alfabetico.directoryFile);
+	}
+
+}
