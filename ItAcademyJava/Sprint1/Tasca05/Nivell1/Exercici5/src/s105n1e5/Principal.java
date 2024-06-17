4038a9c906fb7ee4fbba7384bfdaaea1c898bfe0
davidcuadrado <166518766+davidcuadrado@users.noreply.github.com>
2024-06-17 11:36:48 +0200

    Update Principal.java
    Deleted comments.

--- ItAcademyJava/Sprint1/Tasca05/Nivell1/Exercici5/src/s105n1e5/Principal.java
+++ ItAcademyJava/Sprint1/Tasca05/Nivell1/Exercici5/src/s105n1e5/Principal.java
@@ -7,10 +7,6 @@ public class Principal {
 		Alfabetico.iniciar();
 		Alfabetico alfa1 = new Alfabetico(Alfabetico.directory);
 
-		// Exercici1: Alfabetico.ordenarDirectorio(Alfabetico.directory);
-		// Exercici2: Alfabetico.listarArbolDirectorio(Alfabetico.directoryFile);
-		// Exercici3: Alfabetico.txtArbolDirectorio(Alfabetico.directoryFile);
-		// Exercici4: Alfabetico.readtxt(Alfabetico.directoryFile);
 		Alfabetico.serializar(alfa1);
 		Alfabetico.deserializar(alfa1);
 		
