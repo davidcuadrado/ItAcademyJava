9d2dad41c6badabcaf770714f5ef18b01a69aeae
David <166518766+davidcuadrado@users.noreply.github.com>
2024-09-07 17:14:47 +0200

    Scanner reading update

--- ItAcademyJava/Sprint3/Tasca01/Nivell1/src/s301n1/Menu.java
+++ ItAcademyJava/Sprint3/Tasca01/Nivell1/src/s301n1/Menu.java
@@ -10,26 +10,26 @@ public class Menu {
 		char option = 'X';
 		Command.setInstance();
 		Undo.setInstance();
-		
-		try (Scanner sc = new Scanner(System.in)){
 
-		do {
-			String inputCommand = Command.commandInput(sc);
-			Undo.undoInput(inputCommand);
-			
-			switch (inputCommand) {
-			case "terminate" -> option = '9';
-			case "undo" -> { 
-				option = Undo.getUndoOption(sc);
-				Undo.undoInit(option, sc);
+		try (Scanner sc = new Scanner(System.in)) {
+
+			do {
+				String inputCommand = Command.commandInput(sc);
+				Undo.undoInput(inputCommand);
+
+				switch (inputCommand) {
+				case "terminate" -> option = '9';
+				case "undo" -> {
+					option = Undo.getUndoOption(sc);
+					Undo.undoInit(option, sc);
 				}
-			
-			default -> System.out.println("** Command " + inputCommand + " execution **");
-			
-			}
 
-		} while (option != '9');
-		
+				default -> System.out.println("** Command " + inputCommand + " execution **");
+
+				}
+
+			} while (option != '9');
+
 		} catch (Exception e) {
 			System.out.println("Data entry error. ");
 		}
--- ItAcademyJava/Sprint3/Tasca01/Nivell1/src/s301n1/Principal.java
+++ ItAcademyJava/Sprint3/Tasca01/Nivell1/src/s301n1/Principal.java
@@ -3,9 +3,8 @@ package s301n1;
 public class Principal {
 
 	public static void main(String[] args) {
-	
-			Menu.start();
-		
+
+		Menu.start();
 
 	}
 }
