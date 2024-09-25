6b10d20f8e40f3ee57ca163c1ebc62cbcc0be4be
David <166518766+davidcuadrado@users.noreply.github.com>
2024-09-25 07:48:17 +0200

    Update Principal.java

--- ItAcademyJava/Sprint3/Tasca01/Nivell3/src/s301n3/Principal.java
+++ ItAcademyJava/Sprint3/Tasca01/Nivell3/src/s301n3/Principal.java
@@ -1,21 +1,18 @@
 package s301n3;
 
-import commands.*;
 import vehicles.*;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
 		Initializer initializer = new Initializer();
-		
+
 		Vehicle vehicle = initializer.initVehicle();
-		
-		initializer.initRun(vehicle);
-		
-		//continuar con Vehicle.toString()
-		
-		
+
+		initializer.initStart(vehicle);
+		initializer.initAccelerate(vehicle);
+		initializer.initBreak(vehicle);
 
 	}
 
