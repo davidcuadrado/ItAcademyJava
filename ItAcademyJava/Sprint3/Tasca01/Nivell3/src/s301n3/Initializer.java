e169557ef1d8eff5e52d206228bde1a65af2afb9
David <166518766+davidcuadrado@users.noreply.github.com>
2024-09-25 07:48:45 +0200

    Update Initializer.java

--- ItAcademyJava/Sprint3/Tasca01/Nivell3/src/s301n3/Initializer.java
+++ ItAcademyJava/Sprint3/Tasca01/Nivell3/src/s301n3/Initializer.java
@@ -4,35 +4,44 @@ import commands.*;
 import vehicles.*;
 
 public class Initializer {
-	
-		private Vehicle vehicle;
-	
+
+	private Vehicle vehicle;
+
 	public Vehicle initVehicle() {
 		Car car = new Car("Hyundai", "Getz");
 		Bike bike = new Bike("Peugeot", "Velo");
 		Ship ship = new Ship("Harland & Wolff", "Titanic");
 		Plane plane = new Plane("Boeing", "747");
-		
-		int random = (int) (Math.random()*4+1);
-		switch(random) {
+
+		int random = (int) (Math.random() * 4 + 1);
+		switch (random) {
 		case 1 -> this.vehicle = car;
 		case 2 -> this.vehicle = bike;
 		case 3 -> this.vehicle = ship;
 		case 4 -> this.vehicle = plane;
 		}
-		System.out.println("You are using the " + vehicle.getClass() + " " + vehicle.getBrand() + " " + vehicle.getModel() + " today ");
+		System.out.println("You are using the " + vehicle.getClass() + " " + vehicle.getBrand() + " "
+				+ vehicle.getModel() + " today ");
 		return vehicle;
 	}
-	
-	public static void initRun(Vehicle vehicle) {
+
+	public void initStart(Vehicle vehicle) {
+		Start starter = new Start(vehicle);
+		starter.execute();
+	}
+
+	public void initAccelerate(Vehicle vehicle) {
 		Accelerate accelerator = new Accelerate(vehicle);
 		accelerator.execute();
+	}
+
+	public void initBreak(Vehicle vehicle) {
+		Brake braker = new Brake(vehicle);
+		braker.execute();
 	}
 
 	public Vehicle getVehicle() {
 		return vehicle;
 	}
-	
-	
 
 }
